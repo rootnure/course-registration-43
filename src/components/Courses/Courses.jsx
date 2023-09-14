04c4d9231b689de84f0f8c895d7a7c1ead8a16da
rootnure <nur.diu.2791@gmail.com>
2023-09-14 20:59:13 +0600

    Data load and consoled individually

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,9 +1,18 @@
-const Courses = () => {
+import PropTypes from 'prop-types';
+const Courses = ({courses}) => {
     return (
         <section className='lg:col-span-3'>
-            
+            <div className="grid grid-cols-1 lg:grid-cols-3">
+                {
+                    courses.map(course => console.log(course))
+                }
+            </div>
         </section>
     );
 };
+
+Courses.propTypes = {
+    courses: PropTypes.array.isRequired
+}
 
 export default Courses;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import Courses from "../Courses/Courses";
 import RegistrationSummary from "../RegistrationSummary/RegistrationSummary";
+import { useEffect } from "react";
 
 const Main = () => {
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch('courses.json')
+            .then(res => res.json())
+            .then(data => setCourses(data))
+    }, [])
+
     return (
         <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 container mx-auto">
-            <Courses></Courses>
+            <Courses
+                courses={courses}
+            ></Courses>
             <RegistrationSummary></RegistrationSummary>
         </main>
     );
