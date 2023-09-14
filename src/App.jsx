8fb63639782b3fcc5df9c3bacae97598385bf1d0
rootnure <nur.diu.2791@gmail.com>
2023-09-14 20:05:44 +0600

    Header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
 
 function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <Header></Header>
+      <Main></Main>
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h1 className="text-6xl font-bold text-center py-8">Course Registration</h1>
+        </header>
+    );
+};
+
+export default Header;
